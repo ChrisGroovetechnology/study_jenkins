687d1927853c122fa706d6f49a36e97e4fb96832
GROOVETECH\cuonglam <cuong.lam@groovetechnology.com>
2018-12-20 13:38:10 +0700

    no message

---
+++ study_jenkins.Test/SimpleMathTest.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+
+namespace study_jenkins.Test
+{
+    public class SimpleMathTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            var res = Models.SimpleMath.Add(1, 2);
+            Assert.True(4 == res);
+        }
+        [Fact]
+        public void SubtractTest()
+        {
+            var res = Models.SimpleMath.Subtract(10, 3);
+            Assert.True(7 == res);
+        }
+        [Fact]
+        public void MultiplyTest()
+        {
+            var res = Models.SimpleMath.Multiply(7, 6);
+            Assert.Equal(42, res);
+        }
+        [Fact]
+        public void DivideTest()
+        {
+            var res = Models.SimpleMath.Divide(21, 3);
+            Assert.Equal(7, res);
+        }
+        [Fact]
+        public void RemainderTest()
+        {
+            var res = Models.SimpleMath.Remainder(19, 4);
+            Assert.Equal(3, res);
+        }
+    }
+}
---
+++ study_jenkins/Models/SimpleMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace study_jenkins.Models
+{
+    public class SimpleMath
+    {
+        public static int Add(int a, int b) => a + b; 
+        public static int Subtract(int a, int b) => a - b;
+        public static int Multiply(int a, int b) => a * b;
+        public static int Divide(int a, int b) => a / b;
+        public static int Remainder(int a, int b) => a % b;
+    }
+}
