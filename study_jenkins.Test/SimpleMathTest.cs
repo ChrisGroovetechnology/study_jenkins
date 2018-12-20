1ae469592d38d8f232ab444d1a54a14a492db947
GROOVETECH\cuonglam <cuong.lam@groovetechnology.com>
2018-12-20 13:42:15 +0700

    no message

--- study_jenkins.Test/SimpleMathTest.cs
+++ study_jenkins.Test/SimpleMathTest.cs
@@ -11,24 +11,28 @@ namespace study_jenkins.Test
             var res = Models.SimpleMath.Add(1, 2);
             Assert.True(4 == res);
         }
+
         [Fact]
         public void SubtractTest()
         {
             var res = Models.SimpleMath.Subtract(10, 3);
             Assert.True(7 == res);
         }
+
         [Fact]
         public void MultiplyTest()
         {
             var res = Models.SimpleMath.Multiply(7, 6);
             Assert.Equal(42, res);
         }
+
         [Fact]
         public void DivideTest()
         {
             var res = Models.SimpleMath.Divide(21, 3);
             Assert.Equal(7, res);
         }
+
         [Fact]
         public void RemainderTest()
         {
